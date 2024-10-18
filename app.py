923a5bf559870a866dc40966fa53b8adb4bf89f0
Sevval Mertoglu <sevvalmertoglu131@gmail.com>
2024-10-18 17:56:29 +0300

    added file reading feature

--- app.py
+++ app.py
@@ -34,6 +34,87 @@ def update_history(new_question):
 def get_history():
     return " ".join(user_history)  # Son 5 soruyu birleştirip gönderir
 
+def extract_text_from_docx(docx_path):
+    doc = Document(docx_path)
+    text = []
+    for paragraph in doc.paragraphs:
+        text.append(paragraph.text)
+    return '\n'.join(text)
+
+def read_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def call_claude_sonnet_file(file_path, text):
+    file_extension = os.path.splitext(file_path)[1]
+
+    if file_extension in ['.docx']:
+        text = extract_text_from_docx(file_path)
+    elif file_extension in ['.swift']:
+        text = read_file(file_path)
+    else:
+        return "Unsupported file type."
+
+    bedrock = boto3.client(
+        service_name='bedrock-runtime',
+        region_name=AWS_REGION,
+        aws_access_key_id=AWS_ACCESS_KEY,
+        aws_secret_access_key=AWS_SECRET_KEY
+    )
+
+    model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
+
+    try:
+        response = bedrock.invoke_model(
+            modelId=model_id,
+            body=json.dumps(
+                {
+                    "system": """ Senin adın Coding Buddy, bir yazılım asistanısın.
+                                Senin görevin, kullanıcılara yazılım geliştirme konusunda ve yazılım projelerinde yardımcı olmak.
+                                Aşağıdaki kurallara göre cevap vermelisin:
+                                Yanıt dilin, soru diliyle aynı olsun: Soru hangi dilde sorulduysa o dilde cevap ver. Örneğin ingilizce bir yazı yazıldysa sende cevabını ingilizce ver.
+                                Yazılımcı gibi düşün: Cevaplarında teknik ve çözüm odaklı ol, yazılım geliştirme pratiğine uygun önerilerde bulun.
+                                Arkadaşça bir dil kullan: Resmi olmayan, rahat ve anlaşılır bir dilde konuş. Ancak, saygılı olmayı unutma.
+                                Kapsamını bil: Yalnızca yazılım, projeler ve teknoloji ile ilgili sorulara cevap ver. Yazılım dışı, kişisel, ya da etik olmayan soruları yanıtsız bırak.
+                                Kod örnekleriyle destekle: Mümkün olduğunda verdiğin cevapları gerçek dünyadan kod örnekleri ile destekle. Kodun iyi açıklanmış ve kolay anlaşılır olmasına özen göster.
+                                Yasaklı içeriklere cevap verme: Kural dışı, yasadışı, yasaklı veya şiddet içerikli sorulara asla cevap verme. Bu tür soruları görmezden gel.
+                                Maksimum doğruluk: Cevaplarının doğruluğundan emin ol, yanlış bilgi verme. Eğer emin değilsen veya bilgin yoksa, bunu belirt.
+                                Her zaman 'Merhaba' deme: Her yanıtında "Merhaba" demek zorunda değilsin. Ancak, gerektiğinde sıcak ve arkadaşça bir ton kullanabilirsin.
+                                Örnekler:
+                                Kullanıcı senden bir Python fonksiyonu isterse:
+                                Yanıt: "İşte bir Python fonksiyonu örneği:"
+                                def factorial(n):
+                                    if n == 0:
+                                        return 1
+                                    else:
+                                        return n * factorial(n-1)
+
+                                Kullanıcı bir hata mesajı alırsa:
+                                Yanıt: "Bu hatayı çözmek için şu adımları izleyebilirsin: ..."
+
+                                Yazılım dışı bir soru sorulursa:
+                                Yanıt: Ben bir yazılım asistanıyım,
+                                sadece yazılım ve teknoloji ile ilgili soruları yanıtlıyorum. """,
+                    "anthropic_version": "bedrock-2023-05-31",
+                    "max_tokens": 1024,
+                    "messages": [
+                        {
+                            "role": "user",
+                            "content": [{"type": "text", "text": f"{text}"}],
+                        }
+                    ],
+                }
+            ),
+        )
+
+        result = json.loads(response.get("body").read())
+        output_list = result.get("content", [])
+        return output_list[0]['text']
+
+    except Exception as e:
+        print(str(e))
+        return None
+
 def image_to_base64(image_path):
     with open(image_path, "rb") as img_file:
         image_data = img_file.read()
@@ -155,19 +236,28 @@ def upload():
     message_text = request.form.get('msg')
     image_file = request.files.get('image')
 
+    uploaded_file = request.files.get('project')
+
     update_history(message_text) # Kullanıcının sorusunu kaydeder
 
     if image_file:
-        filename = image_file.filename
+         filename = image_file.filename
+         image_path = os.path.join(UPLOAD_FOLDER, filename)
+         image_file.save(image_path)
 
-        image_path = os.path.join(UPLOAD_FOLDER, filename)
-        image_file.save(image_path)
+         result = call_claude_sonnet(image_path, message_text)
 
-        result = call_claude_sonnet(image_path, message_text)
+    elif uploaded_file:
+        filename = uploaded_file.filename
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        uploaded_file.save(file_path)
+
+        result = call_claude_sonnet_file(file_path, message_text)
     else:
         result = invoke_claude_3_with_text(message_text)
 
     return jsonify({"message": result})
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
